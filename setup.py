f48ac4c462573317d32d6faae74fe0ab9c8a59d3
onoie <onoie3@gmail.com>
2017-07-26 03:19:19 +0900

    add tes

---
+++ setup.py
@@ -0,0 +1,8 @@
+print("setup start")
+#!/usr/bin/env python
+# coding: utf-8
+#import maya.utils;
+#import os, sys
+#import os.path
+#print(os.path.expanduser('~'))
+#maya.utils.executeDeferred('import polynoise')
